4b891ecac2f68cc9a141ad4449a02101125c982e
Kamran0925 <kamranrizwan0925@gmail.com>
2025-07-18 03:41:33 +0500

    Update navbar logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,22 +4,28 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const NavBar = () => (
-  <header className='w-full  absolute z-10'>
-    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
+  <header className="w-full absolute z-10">
+    <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+      <Link
+        href="/"
+        className="flex items-center gap-3 hover:opacity-90 transition-opacity"
+      >
         <Image
-          src='/logo.svg'
-          alt='logo'
-          width={118}
-          height={18}
-          className='object-contain'
+          src="/logo.png"
+          alt="Go Motors Logo"
+          width={50}
+          height={50}
+          className="object-contain"
         />
+        <h4 className="text-2xl font-semibold tracking-tight text-primary-blue">
+          Go Motors
+        </h4>
       </Link>
 
       <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+        title="Sign Up"
+        btnType="button"
+        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
       />
     </nav>
   </header>
