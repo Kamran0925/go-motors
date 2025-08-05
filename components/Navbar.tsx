f05b2e4aadd35d5ef441d38f6123ca0267ffd6a3
Kamran0925 <kamranrizwan0925@gmail.com>
2025-08-05 15:30:00 +0500

    Update navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import CustomButton from "./CustomButton";
-
 const NavBar = () => (
   <header className="w-full absolute z-10">
     <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
@@ -21,12 +19,6 @@ const NavBar = () => (
           Go Motors
         </h4>
       </Link>
-
-      <CustomButton
-        title="Sign Up"
-        btnType="button"
-        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
-      />
     </nav>
   </header>
 );
